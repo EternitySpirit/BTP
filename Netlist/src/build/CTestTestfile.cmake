7edeef1e1cc45525fd90874ba5af74d329e6386e
EternitySpirit <kanishka.mahanty@kgpian.iitkgp.ac.in>
2024-11-02 16:35:51 +0530

    1st Update

---
+++ Netlist/src/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/eternity_spirit/BTP/Netlist/src
+# Build directory: /home/eternity_spirit/BTP/Netlist/src/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_main "/home/eternity_spirit/BTP/Netlist/src/build/bin/main")
+set_tests_properties(test_main PROPERTIES  _BACKTRACE_TRIPLES "/home/eternity_spirit/BTP/Netlist/src/CMakeLists.txt;78;add_test;/home/eternity_spirit/BTP/Netlist/src/CMakeLists.txt;0;")
